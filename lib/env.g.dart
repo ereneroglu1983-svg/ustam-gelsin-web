3ffb2d48743f39bbd0c3c618f5e97e01c44bb258
ereneroglu1983-svg <ereneroglu1983@gmail.com>
2026-07-01 19:11:00 +0300

    Hızlı güncelleme

---
+++ lib/env.g.dart
@@ -0,0 +1,74 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// generated_from: .env
+final class _Env {
+  static const String geminiApiKey =
+      'AQ.Ab8RN6K0GvR3URqTayFP8ALbTsnLmRHtWnDiS3rC7qUte31I4A';
+
+  static const String r2AccessKey = '18936390e3cc8e22d6ad82471f740983';
+
+  static const String r2SecretKey =
+      '3c585c6f9d76587493159351bd4bf5d84278218dfb8ab0a0aacbecd1a84d55b3';
+
+  static const String r2Endpoint =
+      'a645c7b6e357254a62505b1b7c0d8e9a.r2.cloudflarestorage.com';
+
+  static const String firebaseApiKeyAndroid =
+      'AIzaSyD1vi6WcCMAwA_BK_1I9QBUo93480b1V4g';
+
+  static const String firebaseApiKeyWeb =
+      'AIzaSyDb63MIo3CRdKjnLb-bfbnr2N5tHyVa_kE';
+
+  static const String firebaseApiKeyIos =
+      'AIzaSyAbF3WZ7DyYCsiUzEGhO4QJEWSFZ36HdU4';
+
+  static const String adminPhoneToken =
+      'fPIHMfeITTmx9duURXMXwC:APA91bHGFlIyRtxiXjw8xxa5shSvH1LyVK-z-WWK3yn5Hdi8lo6SFEWQtSKKzUc7cRiWhzSxW2Ho1a06vy5Tpbfxj_u_0TZjF4moUXo5WvhesMM_NiO_Du4';
+
+  static const String billingApiUrl =
+      'https://entegrator-api-adresi.com/v1/create-invoice';
+
+  static const String billingApiKey = 'YOUR_API_KEY_HERE';
+
+  static const String igAppId = '1431847685643581';
+
+  static const String igAppSecret = '66b8676e6c67e3eb762eaa2f44264907';
+
+  static const String akbankMerchantId = 'deneme123';
+
+  static const String akbankApiKey = 'deneme123';
+
+  static const String akbankSecretKey = 'deneme123';
+
+  static const String akbankBaseUrl = 'https://entegrasyon.akbank.com/api';
+
+  static const String akbankStoreKey = 'xxx';
+
+  static const String baseAppUrl = 'https://hemenustamgelsin.com';
+
+  static const String r2FlutterAccessKey = '18936390e3cc8e22d6ad82471f740983';
+
+  static const String r2FlutterSecretKey =
+      '9389be4ac0e5b9afaa750d48bd33bce616ef32862fb7b0dfd6d4cf88c67325d5';
+
+  static const String r2FlutterEndpoint =
+      'https://a645c7b6e357254a62505b1b7c0d8e9a.r2.cloudflarestorage.com';
+
+  static const String facebookPageToken =
+      'EAAU4tNYlWjYBR6QOlcTyZAs2EPSskpGYfRkfzfMW4Kk9KBMHjOYCMkmXrfZAoD1cthUBZClZCLBn6uAMWMwKTtPh5ZARBEgzTa9ZAbV2eMpqVr2JNsqkaCWPwZC99vPc5SNTo4eZA75W6p6utOgzvUOGFesbgZB9nnJCF1x7cLOnHWv5ObzUSoHAyw1MLPogwYRiPBLsrOHGSuNkoe3Y4QgZDZD';
+
+  static const String facebookPageId = '1161586913710308';
+
+  static const String instagramBusinessId = '1114242438444639';
+
+  static const String instagramToken =
+      'EAAU4tNYlWjYBRxg7crVZBq6E6PZCZAK3oJEoohtZAt7hSqLAoDlBGt2XVBmCV5Xs96v4y2dRgr628XN0tACInQ77DAbvKvYVrBsQdqh6W8f5578CNvDZCZCGpNrGEqXCX539kW4nhxp7zIcqVqHcJyvjpqP7OSfFYBmolnLRMjyxyPPfXskJ1ozZAs5BCCEgGDcgtpfaZAT2pb8Gf6cHuyueOOha6X2HoRg8BgrhpcPYdeSYfu6G7acvXYBf2gZDZD';
+}
