e230d674ea1ad11a69b944c84e25d242dee69b78
ereneroglu1983-svg <ereneroglu1983@gmail.com>
2026-08-13 06:15:59 +0300

    deploy: Per 13.08.2026  6:15:59,27 - canliya atildi

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2388698298" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4132794601" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
